9e8d1ac4bdb1f1ed123bd8504c4bdb26ef007cfb
Kevin Tovar <kevintovar@esimez.mx>
2018-09-25 09:28:49 -0500

    Add heroku deploy

--- myl/settings/base.py
+++ myl/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ij3!!y$yoz3(q(170lp#ka3thp)y+ofw5p1)o^7pa=&7qzh)##'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- myl/settings/local.py
+++ myl/settings/local.py
@@ -2,10 +2,6 @@ from .base import *
 
 DEBUG = True
 
-INSTALLED_APPS += ['debug_toolbar', ]
-
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
