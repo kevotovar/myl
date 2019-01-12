ceb16a08da42dc195616427307741d5fcc8a3383
Kevin Tovar <kevintovar@esimez.mx>
2019-01-12 09:41:49 -0600

    Add about and update home

--- core/views.py
+++ core/views.py
@@ -6,6 +6,10 @@ def home(request):
     return render(request, 'home.html')
 
 
+def about(request):
+    return render(request, 'about.html')
+
+
 def privacy_policy(request):
     return render(request, 'privacy.html')
 
